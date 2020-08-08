0f7e3ece89df942fb018a3035c143915ddd8696c
HectorSepulveda1 <h.sepulveda01@ufromail.cl>
2020-08-07 23:38:58 -0400

    6° commit: ”Modificacion smallStraight”

--- src/main/java/com/mycompany/generala/Generala.java
+++ src/main/java/com/mycompany/generala/Generala.java
@@ -48,11 +48,12 @@ public class Generala {
         }
         return false;
     }
+
     // Metodo Ones, modificado para funcionar con programación funcional
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
         return addACertainNumber(d1, d2, d3, d4, d5, 1);
     }
-    
+
     // Metodo twoss, modificado para funcionar con programación funcional
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
         return addACertainNumber(d1, d2, d3, d4, d5, 2);
@@ -63,7 +64,7 @@ public class Generala {
         return addACertainNumber(d1, d2, d3, d4, d5, 3);
 
     }
-    
+
     // Metodo fours, modificado para funcionar con programación funcional
     public int fours() {
         return addACertainNumber(4);
@@ -162,23 +163,13 @@ public class Generala {
         }
         return 0;
     }
-
+    
+    //Metodo modificado con segmentación de código
     public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-        int[] tallies;
-        tallies = new int[6];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1
-                && tallies[1] == 1
-                && tallies[2] == 1
-                && tallies[3] == 1
-                && tallies[4] == 1) {
-            return 15;
+        if (thereIsRepetitiveDice(d1, d2, d3, d4, d5) || thereIsADice(d1, d2, d3, d4, d5, 6)) {
+            return 0;
         }
-        return 0;
+        return 15;
     }
 
     //Metodo creado con la adición de los siguientes otros 2 metodos
